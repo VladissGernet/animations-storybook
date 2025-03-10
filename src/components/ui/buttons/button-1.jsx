77d871ed971cf4e354feb04a199bd525fd3c09f2
VladissGernet <skeimorr@gmail.com>
2025-03-10 16:26:51 +0400

    feat: добавил кнопок

--- src/components/ui/buttons/button-1.jsx
+++ src/components/ui/buttons/button-1.jsx
@@ -17,6 +17,27 @@ const Button = () => {
     <button className="button-1 button--4">
       Clip-path 4
     </button>
+    <hr className="line" />
+    <button className="button-1 button--5">
+      Transform 1
+    </button>
+    <button className="button-1 button--6">
+      Transform 2
+    </button>
+    <button className="button-1 button--7">
+      Transform 3
+    </button>
+    <button className="button-1 button--8">
+      Transform 4
+    </button>
+    <hr className="line" />
+    <p style={{color: '#ffffff'}}>
+      При обманчивой на первый взгляд схожести анимаций между ними есть одно существенное отличие:
+      <br />
+      с помощью clip-path мы определяем видимую область блока,
+      <br />
+      а с помощью transform можем менять размер scale(), положение translate() и так далее, но не можем ограничить область видимости.
+    </p>
     </>
   );
 };
