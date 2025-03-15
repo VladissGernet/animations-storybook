ed14ac7b5dbea2c90e6c14833549a48ea3b26f47
VladissGernet <skeimorr@gmail.com>
2025-03-15 17:32:32 +0400

    добавил карточку товара

---
+++ src/components/ui/cards/cards-2.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './cards-2.css';
+
+import cardImage from '/src/assets/tools-card-2.png';
+
+const CardsList2 = () => (
+  <div className='cards-2'>
+    <div className='card' data-brand="Tekton" tabIndex="0">
+      <img className='card__image' src={cardImage} width="350" height="220" alt="Инструменты" />
+      <div className='card__content'>
+        <h2 className='card__title'>Набор инструментов, 65 предметов</h2>
+        <button className='card__button' type="button">Купить</button>
+      </div>
+    </div>
+  </div>
+);
+
+export { CardsList2 };
--- src/components/ui/cards/cards.stories.jsx
+++ src/components/ui/cards/cards.stories.jsx
@@ -4,11 +4,20 @@ import { StyledCards } from './styled';
 
 import { Container } from '/src/components/layout/container/container';
 import { CardsList1 } from './cards-1';
+import { CardsList2 } from './cards-2';
 
 export const Cards1 = () => (
   <StyledCards>
     <Container>
       <CardsList1 />
+    </Container>
+  </StyledCards>
+)
+
+export const Cards2 = () => (
+  <StyledCards>
+    <Container>
+      <CardsList2 />
     </Container>
   </StyledCards>
 )
