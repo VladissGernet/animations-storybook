238ecc74a4f3482a4f0df41c9137d61f4cc9dbd0
VladissGernet <skeimorr@gmail.com>
2025-03-16 13:39:01 +0400

    feat: добавил 3 кнопку

--- src/components/ui/buttons/button-4.jsx
+++ src/components/ui/buttons/button-4.jsx
@@ -6,7 +6,9 @@ const Button4 = () => (
   <div className="button-4">
     <button className="btn btn--1" type="button">Button №1</button>
     <button className="btn btn--2" type="button">Button №2</button>
-    <button className="btn btn--3" type="button">Button №3</button>
+    <button className="btn btn--3" type="button">
+      <span data-hover="hovered">Button №3</span>
+    </button>
     <button className="btn btn--4" type="button">Button №4</button>
     <button className="btn btn--5" type="button">Button №5</button>
   </div>
