097e2930f8f897a4805f8872f6bb0d5da02c68fc
VladissGernet <skeimorr@gmail.com>
2025-03-17 20:24:49 +0400

    feat: вторая кнопка добавлена

--- src/components/ui/buttons/button-5.jsx
+++ src/components/ui/buttons/button-5.jsx
@@ -4,8 +4,11 @@ import './button-5.scss';
 
 const Button5 = () => (
   <div className="button-5">
-    <button className="btn btn-1" data-hover="hovered">
-      <span>Button 5</span>
+    <button className="btn btn-1" data-hover="hovered" type="button">
+      <span>Button 1</span>
+    </button>
+    <button className='btn btn-2'>
+      button 2
     </button>
   </div>
 );
