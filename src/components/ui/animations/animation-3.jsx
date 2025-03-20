2669d81891fc932bdbfe0ae9294ebf41abd6d917
VladissGernet <skeimorr@gmail.com>
2025-03-20 16:58:18 +0400

    3 анимация готова

--- src/components/ui/animations/animation-3.jsx
+++ src/components/ui/animations/animation-3.jsx
@@ -6,12 +6,12 @@ const Animation3 = () => {
   return (
     <>
       <p className="animation-3">
-        <span>
+        <span className="animation-3__text animation-3__text--1" data-text="Glitch is simple">
           Glitch is simple
         </span>
       </p>
       <p className="animation-3">
-        <span>
+        <span className="animation-3__text animation-3__text--2" data-text="Glitch is simple">
           Glitch is simple
         </span>
       </p>
