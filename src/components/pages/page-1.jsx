1c283dec8bb4ff331eaac4a9320abcd21efb3b63
VladissGernet <skeimorr@gmail.com>
2025-03-21 19:06:22 +0400

    подготовка страницы

---
+++ src/components/pages/page-1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './page-1.scss';
+
+const Page1 = () => {
+  return (
+    <div className="page-1">
+      test
+    </div>
+  );
+};
+
+export { Page1 };
+
---
+++ src/components/pages/pages.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from '/src/components/layout/container/container';
+
+import { Page1 } from './page-1';
+
+export const Pages1 = () => (
+  <Container>
+    <Page1 />
+  </Container>
+)
+
+export default {
+  title: 'Страницы'
+}
