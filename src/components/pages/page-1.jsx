939f66bce0548f96933e1c4f85285bd8ca1a2113
VladissGernet <skeimorr@gmail.com>
2025-03-21 19:42:08 +0400

    подготовка страницы для анимации

--- src/components/pages/page-1.jsx
+++ src/components/pages/page-1.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
+import logo from '/src/assets/page-1/logo.svg';
+import menuIcon from '/src/assets/page-1/menu-icon.svg';
+import cartIcon from '/src/assets/page-1/cart-icon.svg';
+import boot from '/src/assets/page-1/boot.png';
+import textCircle from '/src/assets/page-1/text-circle.svg';
+import starIcon from '/src/assets/page-1/star-icon.svg';
+
 import './page-1.scss';
 
 const Page1 = () => {
   return (
-    <div className="page-1">
-      test
+    <div className='page-1'>
+      <header className="header">
+        <div className="header__wrap">
+          <nav className="header__top">
+            <a href="#"><img width="137" height="37" src={logo} alt="logo"/></a>
+            <nav className="header__nav">
+              <a href="#"><img width="30" height="21" src={menuIcon} alt="menu"/></a>
+              <a href="#"><img width="30" height="29" src={cartIcon} alt="cart"/></a>
+            </nav>
+          </nav>
+
+          <div className="header__promo promo">
+            <p className="promo__title">скорость</p>
+            <div className="promo__box">
+              <img className="promo__img" src={boot} alt="boot"/>
+              <img className="promo__badge" width="179" height="179" src={textCircle} alt="защита от дождя и холода"/>
+            </div>
+          </div>
+
+          <div className="header__description">
+            <h1 className="header__title">
+              Премиальные<br/>
+              беговые кеды<br/>
+              для российской осени
+            </h1>
+            <div className="header__rating rating">
+              <img className="rating__item" src={starIcon}  width="24" height="24" alt="star"/>
+              <img className="rating__item" src={starIcon}  width="24" height="24" alt="star"/>
+              <img className="rating__item" src={starIcon}  width="24" height="24" alt="star"/>
+              <img className="rating__item" src={starIcon}  width="24" height="24" alt="star"/>
+              <img className="rating__item" src={starIcon}  width="24" height="24" alt="star"/>
+            </div>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
--- src/components/pages/pages.stories.jsx
+++ src/components/pages/pages.stories.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import { Container } from '/src/components/layout/container/container';
-
 import { Page1 } from './page-1';
 
 export const Pages1 = () => (
-  <Container>
-    <Page1 />
-  </Container>
+  <Page1 />
 )
 
 export default {
