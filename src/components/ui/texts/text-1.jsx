c24437d0fff652a10e346f2a60b64cc95eac5b33
VladissGernet <skeimorr@gmail.com>
2025-03-21 17:48:13 +0400

    бегущая строка добавлена

--- src/components/ui/texts/text-1.jsx
+++ src/components/ui/texts/text-1.jsx
@@ -4,7 +4,9 @@ import './text-1.scss';
 
 const Text1 = () => {
   return (
-    <div className="text-1">TEst</div>
+    <div className="text-1">
+      <span className='text-1__text'>Анимация бегущей строки, много много текста</span>
+    </div>
   );
 };
 
--- src/components/ui/texts/texts.stories.jsx
+++ src/components/ui/texts/texts.stories.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { Container } from '/src/components/layout/container/container';
-
 import { StyledTexts } from './styled';
 
 import { Text1 } from './text-1';
 
 export const Animations1 = () => (
   <StyledTexts>
-    <Container>
-      <Text1 />
-    </Container>
+    <Text1 />
   </StyledTexts>
 )
 
