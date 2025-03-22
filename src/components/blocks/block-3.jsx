f7c16965c232f8ca16b2817da64ad8433b61a15e
VladissGernet <skeimorr@gmail.com>
2025-03-22 21:06:02 +0400

    добавил еще код из гайда

--- src/components/blocks/block-3.jsx
+++ src/components/blocks/block-3.jsx
@@ -56,3 +56,32 @@ const Block3 = () => {
 };
 
 export { Block3 };
+
+// Это код из гайда.
+
+// ("use strict");
+
+// const sliderBox = document.querySelector(".slider");
+// const slides = sliderBox.querySelectorAll(".slider-item");
+// const btnPrev = sliderBox.querySelector(".slider-btn--prev");
+// const btnNext = sliderBox.querySelector(".slider-btn--next");
+
+// let counter = 0;
+
+// btnNext.addEventListener("click", function () {
+//   slides[counter].classList.toggle("slider-item--current");
+//   counter++;
+//   if (counter >= slides.length) {
+//     counter = 0;
+//   }
+//   slides[counter].classList.toggle("slider-item--current");
+// });
+
+// btnPrev.addEventListener("click", function () {
+//   slides[counter].classList.toggle("slider-item--current");
+//   counter--;
+//   if (counter < 0) {
+//     counter = slides.length - 1;
+//   }
+//   slides[counter].classList.toggle("slider-item--current");
+// });
