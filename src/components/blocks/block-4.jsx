1d652b6eae10a8baf9176c2c3042908472e94820
VladissGernet <skeimorr@gmail.com>
2025-03-22 20:25:14 +0400

    логика добавлена

---
+++ src/components/blocks/block-4.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+
+import image1 from "/src/assets/block-3/slide-1.jpg";
+import image2 from "/src/assets/block-3/slide-2.jpg";
+import image3 from "/src/assets/block-3/slide-3.jpg";
+
+const sliders = [
+  {
+    image: image1,
+    line1: "Производство",
+    line2: "волшебных изделий",
+    line3: "из металла и дерева",
+  },
+  {
+    image: image2,
+    line1: "Производство",
+    line2: "волшебных изделий",
+    line3: "из металла и дерева",
+  },
+  {
+    image: image3,
+    line1: "Производство",
+    line2: "волшебных изделий",
+    line3: "из металла и дерева",
+  },
+];
+
+import arrow from "/src/assets/block-3/arrow.svg";
+
+import "./block-4.scss";
+
+const defineSliderClass = (index, sliderIndex, sliders) => {
+  if (index === sliderIndex) {
+    return " slider-item--current";
+  }
+  if (index === sliderIndex - 1 && sliderIndex > 0) {
+    return " slider-item--prev";
+  }
+  if (index === sliderIndex + 1 && sliderIndex < sliders.length - 1) {
+    return " slider-item--next";
+  }
+  return "";
+};
+
+const Block4 = () => {
+  const [sliderIndex, setSliderIndex] = useState(0);
+
+  const onClickPrev = () =>
+    setSliderIndex(sliderIndex > 0 ? sliderIndex - 1 : 0);
+
+  const onClickNext = () =>
+    setSliderIndex(
+      sliderIndex < sliders.length - 1 ? sliderIndex + 1 : sliders.length - 1
+    );
+
+  return (
+    <div className="block-4 wrap">
+      <section className="slider">
+        {sliders.map((slider, index) => (
+          <div
+            key={slider.image}
+            className={
+              "slider-item" + defineSliderClass(index, sliderIndex, sliders)
+            }
+          >
+            <img className="slider-img" src={slider.image} alt="slide" />
+            <h3 className="slider-title">
+              <span className="slider-title-line">{slider.line1}</span>
+              <span className="slider-title-line">{slider.line2}</span>
+              <span className="slider-title-line">{slider.line3}</span>
+            </h3>
+          </div>
+        ))}
+
+        <button
+          type="button"
+          className="slider-btn slider-btn--prev"
+          onClick={onClickPrev}
+          disabled={sliderIndex === 0}
+        >
+          <img src={arrow} alt="prev" />
+        </button>
+        <button
+          type="button"
+          className="slider-btn slider-btn--next"
+          onClick={onClickNext}
+          disabled={sliderIndex === sliders.length - 1}
+        >
+          <img src={arrow} alt="next" />
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export { Block4 };
--- src/components/blocks/blocks.stories.jsx
+++ src/components/blocks/blocks.stories.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import { Block1 } from "./block-1";
 import { Block2 } from "./block-2";
 import { Block3 } from "./block-3";
+import { Block4 } from "./block-4";
 
 export const Blocks1Modal = () => <Block1 />;
-
 export const Blocks2MobileBurger = () => <Block2 />;
-export const Blocks3Slider = () => <Block3 />;
+export const Blocks3Slider1 = () => <Block3 />;
+export const Blocks4Slider2 = () => <Block4 />;
 
 export default {
   title: "Блоки",
