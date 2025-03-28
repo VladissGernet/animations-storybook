1ec47cc55cd91555c6276a1fecd03814044ee597
VladissGernet <skeimorr@gmail.com>
2025-03-28 14:21:49 +0400

    feat: подготовка progressbar'a

--- src/components/scrolls/scrolls.stories.jsx
+++ src/components/scrolls/scrolls.stories.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import { Scroll1 } from "./scroll-1";
+import { Scroll2 } from "./scroll-2";
 
 export const Scrolls1 = () => <Scroll1 />;
+export const Scrolls2ProgressBar = () => <Scroll2 />;
 
 export default {
   title: "Прокрутки",
