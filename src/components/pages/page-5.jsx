70e844710e1364eca9361867c0f6980e5bef56e7
VladissGernet <skeimorr@gmail.com>
2025-04-20 14:16:35 +0400

    feat: подготовка страницы для анимации

--- src/components/pages/page-5.jsx
+++ src/components/pages/page-5.jsx
@@ -5,21 +5,26 @@ import "./page-5.scss";
 import bottom from "/src/assets/page-5/bottom.png";
 import clouds from "/src/assets/page-5/clouds.png";
 import greenhouse from "/src/assets/page-5/greenhouse.png";
-import greenhouseSvg from "/src/assets/page-5/greenhouse.svg";
 import house from "/src/assets/page-5/house.png";
-import parallax1 from "/src/assets/page-5/parallax-1.png";
-import parallax2 from "/src/assets/page-5/parallax-2.png";
-import parallaxBg from "/src/assets/page-5/parallax-bg.png";
 
 const Page5 = () => {
   return (
     <div className="page-5">
       <div className="parallax">
         <div className="parallax__assets">
-          <div className="parallax__layer parallax__layer--1"></div>
-          <div className="parallax__layer parallax__layer--2"></div>
-          <div className="parallax__layer parallax__layer--3"></div>
-          <div className="parallax__layer parallax__layer--4"></div>
+          <img
+            className="parallax__layer parallax__layer--clouds"
+            src={clouds}
+          />
+          <img
+            className="parallax__layer parallax__layer--text"
+            src={greenhouse}
+          />
+          <img className="parallax__layer parallax__layer--house" src={house} />
+          <img
+            className="parallax__layer parallax__layer--bottom"
+            src={bottom}
+          />
         </div>
       </div>
     </div>
