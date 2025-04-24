2a2e30a61847c0282ef706ad5ab5571e180f7a3b
VladissGernet <skeimorr@gmail.com>
2025-04-24 10:33:23 +0400

    feat: задание выполенено

--- src/components/ui/buttons/button-6.jsx
+++ src/components/ui/buttons/button-6.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import "./button-6.scss";
 
-import iconCalendar from "/src/assets/button-6/icon-calendar.svg";
-
 const Button6 = () => {
   return (
     <div className="button-6">
@@ -118,7 +116,42 @@ const Button6 = () => {
           </svg>
         </div>
         <div className="icon-wrapper calendar">
-          <img src={iconCalendar} alt="Иконка календаря" />
+          <svg
+            width="60"
+            height="60"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M16 2V6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M8 2V6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M3 10H21"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
         </div>
       </div>
     </div>
