b0af3661284643799e399e49689a011d2391381f
VladissGernet <skeimorr@gmail.com>
2025-04-24 08:47:42 +0400

    feat: первая иконка готова

--- src/components/ui/buttons/buttons.stories.jsx
+++ src/components/ui/buttons/buttons.stories.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from '/src/components/layout/container/container';
+import { Container } from "/src/components/layout/container/container";
 
-import { StyledButtons } from './styled';
+import { StyledButtons } from "./styled";
 
-import { Button1 } from './button-1';
-import { Button2 } from './button-2';
-import { Button3 } from './button-3';
-import { Button4 } from './button-4';
-import { Button5 } from './button-5';
+import { Button1 } from "./button-1";
+import { Button2 } from "./button-2";
+import { Button3 } from "./button-3";
+import { Button4 } from "./button-4";
+import { Button5 } from "./button-5";
+import { Button6 } from "./button-6";
 
 export const Buttons1 = () => (
   <StyledButtons>
@@ -16,7 +17,7 @@ export const Buttons1 = () => (
       <Button1 />
     </Container>
   </StyledButtons>
-)
+);
 
 export const Buttons2 = () => (
   <StyledButtons>
@@ -24,7 +25,7 @@ export const Buttons2 = () => (
       <Button2 />
     </Container>
   </StyledButtons>
-)
+);
 
 export const Buttons3 = () => (
   <StyledButtons>
@@ -32,7 +33,7 @@ export const Buttons3 = () => (
       <Button3 />
     </Container>
   </StyledButtons>
-)
+);
 
 export const Buttons4 = () => (
   <StyledButtons $backgroundColor="#ffffff">
@@ -40,7 +41,7 @@ export const Buttons4 = () => (
       <Button4 />
     </Container>
   </StyledButtons>
-)
+);
 
 export const Buttons5 = () => (
   <StyledButtons $backgroundColor="#ffffff">
@@ -48,8 +49,16 @@ export const Buttons5 = () => (
       <Button5 />
     </Container>
   </StyledButtons>
-)
+);
+
+export const Buttons6 = () => (
+  <StyledButtons $backgroundColor="#ffffff">
+    <Container>
+      <Button6 />
+    </Container>
+  </StyledButtons>
+);
 
 export default {
-  title: 'Кнопки'
-}
+  title: "Кнопки",
+};
