6a2df5632584de8ff9c0e6b05cf8d7e31e1abfff
VladissGernet <skeimorr@gmail.com>
2025-04-29 18:39:09 +0400

    feat: закончил с лоадерами

--- src/components/svg/svg.stories.jsx
+++ src/components/svg/svg.stories.jsx
@@ -6,11 +6,11 @@ import { Svg3 } from "./svg-3";
 import { Svg4 } from "./svg-4";
 import { Svg5 } from "./svg-5";
 
-export const LikeButton = () => <Svg1 />;
-export const Loaders = () => <Svg2 />;
-export const Loader = () => <Svg3 />;
-export const Loader2 = () => <Svg4 />;
-export const Loader3 = () => <Svg5 />;
+export const Svg1LikeButton = () => <Svg1 />;
+export const Svg2Loaders = () => <Svg2 />;
+export const Svg3Loader = () => <Svg3 />;
+export const Svg4Loader2 = () => <Svg4 />;
+export const Svg5Loader3 = () => <Svg5 />;
 
 export default {
   title: "SVG Анимации",
