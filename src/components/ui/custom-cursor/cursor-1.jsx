3b0b110656c0a66ea98b27e2061e0e24706181f1
VladissGernet <skeimorr@gmail.com>
2025-05-06 10:01:01 +0400

    feat: изменение цвета по клику

--- src/components/ui/custom-cursor/cursor-1.jsx
+++ src/components/ui/custom-cursor/cursor-1.jsx
@@ -80,14 +80,14 @@ const Cursor1 = () => {
 
   // вспомогательные функции
 
-  const getRandomElement = (array) =>
-    array[getRandomInteger(0, array.length - 1)];
-
   const getRandomInteger = (a = 0, b = 1) => {
     const lower = Math.ceil(Math.min(a, b));
     const upper = Math.floor(Math.max(a, b));
     return Math.floor(lower + Math.random() * (upper - lower + 1));
   };
+
+  const getRandomElement = (array) =>
+    array[getRandomInteger(0, array.length - 1)];
 
   const changeColor = () => {
     const color = getRandomElement(COLORS);
--- src/components/ui/custom-cursor/cursor-2.jsx
+++ src/components/ui/custom-cursor/cursor-2.jsx
@@ -5,6 +5,7 @@ import "./cursor-2.scss";
 const Cursor2 = () => {
   const isActivePage = useRef(true);
   const cursor = useRef(null);
+  let activeColor;
 
   const PARAMS = {
     speed: 0.2,
@@ -48,6 +49,28 @@ const Cursor2 = () => {
     cursor.current.style.transform = translate;
   };
 
+  // Случайное число.
+
+  const getRandomInteger = (a = 0, b = 1) => {
+    const lower = Math.ceil(Math.min(a, b));
+    const upper = Math.floor(Math.max(a, b));
+    return Math.floor(lower + Math.random() * (upper - lower + 1));
+  };
+
+  // Случайный цвет.
+  const getRandomElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+  const changeColorHandler = () => {
+    const color = getRandomElement(COLORS);
+
+    if (color === activeColor) {
+      changeColorHandler();
+    } else {
+      cursor.current.style.color = color;
+      activeColor = color;
+    }
+  };
+
   const requestAnimationHandler = () => {
     // Остановка цикла анимаций requestAnimationFrame при размонтировании компонента.
     if (isActivePage.current === false) {
@@ -61,10 +84,12 @@ const Cursor2 = () => {
 
   useEffect(() => {
     window.addEventListener("mousemove", updateCoordinates);
+    window.addEventListener("click", changeColorHandler);
     PARAMS.offset = cursor.current.getBoundingClientRect().width / 2;
 
     return () => {
       window.removeEventListener("mousemove", updateCoordinates);
+      window.removeEventListener("click", changeColorHandler);
       // Остановка цикла анимаций requestAnimationFrame при размонтировании компонента.
       isActivePage.current = false;
     };
@@ -83,23 +108,23 @@ const Cursor2 = () => {
       >
         <path
           d="M26.7344 25.4514L21.4005 20.1158L25.1363 16.3801C25.3524 16.1642 25.4452 15.8538 25.3831 15.5548C25.3211 15.2556 25.1128 15.0078 24.8288 14.8955L10.3436 9.16435C10.0074 9.03108 9.62417 9.11065 9.36851 9.3663C9.11286 9.62195 9.03341 10.0051 9.16644 10.3414L14.8969 24.8264C15.0093 25.1106 15.2572 25.3189 15.5561 25.3809C15.6175 25.3936 15.6791 25.3998 15.7403 25.3998C15.9784 25.3998 16.2099 25.3061 16.3816 25.1342L20.1178 21.3982L25.4517 26.7339C25.6287 26.9111 25.8609 26.9996 26.0931 26.9996C26.3252 26.9996 26.5573 26.9111 26.7344 26.734C27.0885 26.3799 27.0886 25.8056 26.7344 25.4514ZM16.0756 22.8748L11.6238 11.6216L22.877 16.0739L16.0756 22.8748Z"
-          fill="black"
+          fill="currentColor"
         />
         <path
           d="M3.02707 2.97021L6.69115 6.37234"
-          stroke="black"
+          stroke="currentColor"
           strokeWidth="1.5"
           strokeLinecap="round"
         />
         <path
           d="M10 1V6"
-          stroke="black"
+          stroke="currentColor"
           strokeWidth="1.5"
           strokeLinecap="round"
         />
         <path
           d="M1 10H6"
-          stroke="black"
+          stroke="currentColor"
           strokeWidth="1.5"
           strokeLinecap="round"
         />
