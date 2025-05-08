5b681827dc00c3aaad91b1741b8c62a8cab5b756
VladissGernet <skeimorr@gmail.com>
2025-05-08 17:15:48 +0400

    feat: checkpoint

--- src/components/svg/svg.stories.jsx
+++ src/components/svg/svg.stories.jsx
@@ -5,12 +5,14 @@ import { Svg2 } from "./svg-2";
 import { Svg3 } from "./svg-3";
 import { Svg4 } from "./svg-4";
 import { Svg5 } from "./svg-5";
+import { Svg6 } from "./svg-6";
 
 export const Svg1LikeButton = () => <Svg1 />;
 export const Svg2Loaders = () => <Svg2 />;
 export const Svg3Loader = () => <Svg3 />;
 export const Svg4Loader2 = () => <Svg4 />;
 export const Svg5Loader3 = () => <Svg5 />;
+export const Svg6morphing = () => <Svg6 />;
 
 export default {
   title: "SVG Анимации",
