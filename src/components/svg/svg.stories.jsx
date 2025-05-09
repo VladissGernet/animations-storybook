51c41bfffb76e56d34200c91a06c3aacfb9308b6
VladissGernet <skeimorr@gmail.com>
2025-05-09 08:07:23 +0400

    feat: подготовка к выполнению задания

--- src/components/svg/svg.stories.jsx
+++ src/components/svg/svg.stories.jsx
@@ -6,13 +6,15 @@ import { Svg3 } from "./svg-3";
 import { Svg4 } from "./svg-4";
 import { Svg5 } from "./svg-5";
 import { Svg6 } from "./svg-6";
+import { Svg7 } from "./svg-7";
 
 export const Svg1LikeButton = () => <Svg1 />;
 export const Svg2Loaders = () => <Svg2 />;
 export const Svg3Loader = () => <Svg3 />;
 export const Svg4Loader2 = () => <Svg4 />;
 export const Svg5Loader3 = () => <Svg5 />;
-export const Svg6morphing = () => <Svg6 />;
+export const Svg6Morphing = () => <Svg6 />;
+export const Svg7Morphing = () => <Svg7 />;
 
 export default {
   title: "SVG Анимации",
