1c48390ed1fc018cec8f4d3bfed724c4b8bfdeaa
VladissGernet <skeimorr@gmail.com>
2025-05-19 17:14:24 +0400

    feat: подготовка задания

---
+++ src/components/scrolls/scroll-13.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./scroll-13.scss";
+
+const Scroll13 = () => {
+  return (
+    <div className="scroll-13">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="507"
+        height="813"
+        viewBox="0 0 507 813"
+        fill="none"
+      >
+        <path
+          className="path"
+          d="M75.3329 2C75.3329 2 308.069 48.4013 317.834 115.968C327.6 183.535 41.9686 227.494 63.9402 329.252C85.9118 431.009 335.737 427.753 369.915 308.086C404.093 188.419 273.077 128.993 103.001 146.902C-67.0758 164.812 4.53545 596.263 120.904 651.619C237.272 706.975 254.361 461.129 361.778 454.617C469.195 448.104 179.495 713.487 342.247 796.521C505 879.555 505 578.353 505 578.353"
+          stroke="white"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeDasharray="6 6"
+        />
+        <circle className="target" r="30" cx="0" cy="0" fill="#ffffff"></circle>
+      </svg>
+    </div>
+  );
+};
+
+export { Scroll13 };
