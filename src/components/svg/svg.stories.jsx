56267da5606ed6af439f3a2920b0c55c615cd5bd
VladissGernet <skeimorr@gmail.com>
2025-05-20 17:28:16 +0400

    feat: добавил комопзицию

--- src/components/svg/svg.stories.jsx
+++ src/components/svg/svg.stories.jsx
@@ -9,6 +9,7 @@ import { Svg6 } from "./svg-6";
 import { Svg7 } from "./svg-7";
 import { Svg8 } from "./svg-8";
 import { Svg9 } from "./svg-9";
+import { Svg10 } from "./svg-10";
 
 export const Svg1LikeButton = () => <Svg1 />;
 export const Svg2Loaders = () => <Svg2 />;
@@ -19,6 +20,7 @@ export const Svg6Morphing = () => <Svg6 />;
 export const Svg7Morphing = () => <Svg7 />;
 export const Svg8Showcase = () => <Svg8 />;
 export const Svg9Text = () => <Svg9 />;
+export const Svg10TimelineAndComposition = () => <Svg10 />;
 
 export default {
   title: "SVG Анимации",
