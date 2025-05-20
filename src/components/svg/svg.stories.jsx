2f78f7ec79e6a90700ac41548ea6b0a8ba207ab7
VladissGernet <skeimorr@gmail.com>
2025-05-20 16:34:40 +0400

    feat: добаивл реализацию текста

--- src/components/svg/svg.stories.jsx
+++ src/components/svg/svg.stories.jsx
@@ -8,6 +8,7 @@ import { Svg5 } from "./svg-5";
 import { Svg6 } from "./svg-6";
 import { Svg7 } from "./svg-7";
 import { Svg8 } from "./svg-8";
+import { Svg9 } from "./svg-9";
 
 export const Svg1LikeButton = () => <Svg1 />;
 export const Svg2Loaders = () => <Svg2 />;
@@ -17,6 +18,7 @@ export const Svg5Loader3 = () => <Svg5 />;
 export const Svg6Morphing = () => <Svg6 />;
 export const Svg7Morphing = () => <Svg7 />;
 export const Svg8Showcase = () => <Svg8 />;
+export const Svg9Text = () => <Svg9 />;
 
 export default {
   title: "SVG Анимации",
