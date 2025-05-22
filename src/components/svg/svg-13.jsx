de33c306a6ff565c05b9300e1a144c12d51cadd1
VladissGernet <skeimorr@gmail.com>
2025-05-22 15:51:38 +0400

    feat: решение чере scss

--- src/components/svg/svg-13.jsx
+++ src/components/svg/svg-13.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
+import anime from "animejs";
 
 import image from "/src/assets/svg-13/image.jpg";
 
@@ -12,31 +13,47 @@ import "./svg-13.scss";
 */
 
 const Svg13 = () => {
+  const mask = useRef(null);
+
+  const paintPreview = useCallback(() => {
+    if (!mask.current) return;
+    const maskCircles = mask.current.querySelectorAll("circle");
+
+    // anime({
+    //   targets: maskCircles,
+    //   r: ["0", "100"],
+    //   duration: 1000,
+    //   easing: "easeInOutQuad",
+    //   fill: "#000000",
+    //   delay: anime.stagger(100),
+    // });
+  }, []);
+
+  const resetPreview = useCallback(() => {
+    if (!mask.current) return;
+    const maskCircles = mask.current.querySelectorAll("circle");
+
+    // anime({
+    //   targets: maskCircles,
+    //   r: ["100", "0"],
+    //   duration: 1000,
+    //   easing: "easeInOutQuad",
+    //   fill: "transparent",
+    //   delay: anime.stagger(100),
+    // });
+  }, []);
+
   return (
     <div className="svg-13">
       <div className="wrapper">
-        {/*
-          Маска отображается частично, потому что в разметке она стоит отдельно от блока preview. И по дефолту элемент SVG, в котором находится маска, имеет размеры 300 x 150. Эту область мы и видим.
-
-          Чтобы исправить это поведение, зададим маске размеры через CSS.
-        */}
-        <div className="visually-hidden">
-          <svg className="mask-wrapper" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <mask id="mask" x="0" y="0" maskUnits="userSpaceOnUse">
-                <rect width="100%" height="100%" fill="#1f1f1f" />
-                <circle cx="20%" cy="50%" r="20%" fill="blue" />
-                <circle cx="50%" cy="50%" r="20%" fill="yellow" />
-                <circle cx="80%" cy="50%" r="20%" fill="red" />
-              </mask>
-            </defs>
-          </svg>
-        </div>
-
-        <div className="preview">
+        <div
+          className="preview"
+          onMouseEnter={() => paintPreview()}
+          onMouseLeave={() => resetPreview()}
+        >
           <img src={image} alt="Preview" />
           <span>Preview</span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg">
             <rect
               x="0"
               y="0"
@@ -45,6 +62,24 @@ const Svg13 = () => {
               fill="currentColor"
               mask="url(#mask)"
             />
+          </svg>
+        </div>
+
+        {/*
+          Маска отображается частично, потому что в разметке она стоит отдельно от блока preview. И по дефолту элемент SVG, в котором находится маска, имеет размеры 300 x 150. Эту область мы и видим.
+
+          Чтобы исправить это поведение, зададим маске размеры через CSS.
+        */}
+        <div className="visually-hidden">
+          <svg className="mask-wrapper" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <mask ref={mask} id="mask" x="0" y="0" maskUnits="userSpaceOnUse">
+                <rect width="100%" height="100%" fill="#ffffff" />
+                <circle cx="20%" cy="30%" r="0" fill="transparent"></circle>
+                <circle cx="85%" cy="75%" r="0" fill="transparent"></circle>
+                <circle cx="40%" cy="30%" r="0" fill="transparent"></circle>
+              </mask>
+            </defs>
           </svg>
         </div>
       </div>
