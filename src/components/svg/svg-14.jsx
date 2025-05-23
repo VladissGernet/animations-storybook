bac80539c77ecf598a2b60c93e87f42807f631fe
VladissGernet <skeimorr@gmail.com>
2025-05-23 16:04:05 +0400

    fix: испавил опечатки

--- src/components/svg/svg-14.jsx
+++ src/components/svg/svg-14.jsx
@@ -119,9 +119,9 @@ body {
             <svg xmlns="http://www.w3.org/2000/svg">
                 <defs>
                     <filter id="gooey-filter">
-                        <fegaussianblur in="SourceGraphic" stdDeviation="10"
-                            resilt="blur" />
-                        <fecolormatrix in="blur" mode="matrix"
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="10"
+                            result="blur" />
+                        <feColorMatrix in="blur" mode="matrix"
                             values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -7"
                             result="goo" />
                     </filter>
